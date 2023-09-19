1af74aa9e51cd46eeb210303ff3dbb385acbab6d
David Märzendorfer <maerzyd@gmail.com>
2023-09-19 12:53:42 +0200

    minor health changes

--- TaTT/Assets/02_Healthbar/Scripts/Runtime/Billboard.cs
+++ TaTT/Assets/02_Healthbar/Scripts/Runtime/Billboard.cs
@@ -6,6 +6,14 @@ public class Billboard : MonoBehaviour
 {
     public Transform cam;
 
+    private void Awake()
+    {
+        if (cam == null)
+        {
+            cam = Camera.main.transform;
+        }
+    }
+
     void LateUpdate()
     {
         transform.LookAt(transform.position + cam.forward);
