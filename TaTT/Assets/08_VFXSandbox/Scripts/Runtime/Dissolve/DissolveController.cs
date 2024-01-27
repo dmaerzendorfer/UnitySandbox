f30142abfd1f5e5f3c3d834846392b5ec87f06e6
David Märzendorfer <maerzyd@gmail.com>
2024-01-27 19:40:16 +0100

    some vfx funnery

--- TaTT/Assets/08_VFXSandbox/Scripts/Runtime/Dissolve/DissolveController.cs
+++ TaTT/Assets/08_VFXSandbox/Scripts/Runtime/Dissolve/DissolveController.cs
@@ -10,22 +10,32 @@ public class DissolveController : MonoBehaviour
     private const string FlipAnimationTrigger = "Flip";
     private static readonly int FlipId = Animator.StringToHash(FlipAnimationTrigger);
 
+    private const string ParticlesNumber = "ParticlesNumber";
+
 
     public SkinnedMeshRenderer skinnedMesh;
     public float dissolveRate = 0.0125f;
     public float refreshRate = 0.025f;
-
+    public float delay = 0.1f;
+    
     public Animator animator;
 
-    public VisualEffect vfx;
+    public VisualEffect vfxDissolve;
+    public VisualEffect vfxMesh;
 
     private Material[] _skinnedMaterials;
+    private float startMeshParticleCount;
 
     // Start is called before the first frame update
     void Start()
     {
         if (skinnedMesh != null)
             _skinnedMaterials = skinnedMesh.materials;
+
+        if (vfxMesh)
+        {
+            startMeshParticleCount = vfxMesh.GetFloat(ParticlesNumber);
+        }
     }
 
     // Update is called once per frame
@@ -44,20 +54,23 @@ public class DissolveController : MonoBehaviour
             {
                 _skinnedMaterials[i].SetFloat(DissolveAmountId, 0);
             }
-            vfx.Stop();
+
+            vfxDissolve.Stop();
+            vfxMesh.SetFloat(ParticlesNumber, startMeshParticleCount);
         }
     }
 
     private IEnumerator DissolveCo()
     {
+        yield return new WaitForSeconds(delay);
         if (animator)
         {
             animator.SetTrigger(FlipId);
         }
 
-        if (vfx != null)
+        if (vfxDissolve != null)
         {
-            vfx.Play();
+            vfxDissolve.Play();
         }
 
         float counter = 0;
@@ -71,8 +84,15 @@ public class DissolveController : MonoBehaviour
                     _skinnedMaterials[i].SetFloat(DissolveAmountId, counter);
                 }
 
+                var count = Remap(counter, 0, 1, startMeshParticleCount, 0);
+                vfxMesh.SetFloat(ParticlesNumber, count);
                 yield return new WaitForSeconds(refreshRate);
             }
         }
+    }
+
+    private static float Remap(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
     }
 }
